4ebb0d3f98f5b76d7dfebcd5d1350cf30aa7a695
Phat <nguyenphatit.nl@gmail.com>
2022-05-22 17:09:03 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Layout from "./components/Layout";
+import RequireAuth from "./components/RequireAuth";
+import { AuthProvider } from "./context/AuthContext";
+import theme from "./theme";
+
+const Home = lazy(() => import("./pages/home"));
+const Login = lazy(() => import("./pages/login"));
+const Manatee = lazy(() => import("./pages/manatee"));
+
+const App = () => {
+  const renderRoutes = () => {
+    return (
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<RequireAuth children={<Home />} />} />
+          <Route
+            path="/manatee"
+            element={<RequireAuth children={<Manatee />} />}
+          />
+        </Route>
+      </Routes>
+    );
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthProvider>
+        <Suspense fallback={<p>Loading...</p>}>{renderRoutes()}</Suspense>
+      </AuthProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
